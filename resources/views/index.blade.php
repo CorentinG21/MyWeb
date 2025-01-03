16529baf3ac1c810f762b3f698441ebda26343e8
Corentin Godon <corentingodon21@gmail.com>
2025-01-04 00:14:59 +0100

    Ajout d'un nouveau design pour la page d'accueil avec une section CV, incluant des styles CSS et un lien de téléchargement pour le CV.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,11 +6,62 @@
     <title>Accueil</title>
     <style>
         body {
-            background-color: rgb(21, 32, 43)
+            background-color: rgb(21, 32, 43);
+        }
+
+        .CV {
+            font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .Rectangle1 {
+            margin-top: 20px;
+            padding: 20px;
+            width: auto;
+            max-width: 1000px;
+            height: auto;
+            background-color: whitesmoke;
+            border-radius: 10px;
+        }
+
+        .header-cv {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding: 20px;
+            width: auto;
+            height: auto;
+            background-color: lightgray;
+            border-radius: 10px;
+        }
+
+        .content-header-cv {
+            display: flex;
+            justify-content: space-between;
         }
     </style>
 </head>
 <body>
     <x-header />
+
+
+    <div class="CV">
+        <div class="Rectangle1">
+            <div class="header-cv">
+                <div class="content-header-cv">
+                    <img src="{{ asset('Image/20230715_063227.jpg') }}" alt="photo" style="width: 250px; height: auto; border-radius: 5px; padding-right: 15px;">
+                    <div class="text-header-cv">
+                        <h1>Corentin GODON</h1>
+                        <h3>Stage 3ème année Bachelor Développement Web Ecole IRIS Rouen</h3>
+                        <span>Diplômé d’un BTS SIO (Services Informatiques aux Organisations), spécialité SLAM ( Solutions logicielles et applications métiers ).</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="DL">
+            <a href="{{ asset('public\Fichier\Godon_Corentin_CV_Stage_IRIS.pdf') }}" download="pdf"><i class="bi bi-download"></i></a>
+        </div>
+    </div>
 </body>
 </html>
